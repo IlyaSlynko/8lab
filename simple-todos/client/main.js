cf778935d1df36acea7bd7a0e041686666a9a022
IlyaSlynko <55802562+IlyaSlynko@users.noreply.github.com>
2020-04-22 16:52:45 +0300

    2
    point 2

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,11 +1,11 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+ 
+import App from '../imports/ui/App.js';
+ 
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
 });
 
 Template.hello.helpers({
